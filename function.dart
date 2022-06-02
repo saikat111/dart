86c177573a35ff45a44c2f5d089594b6a2307e6c
saikat111 <s8856010@gmail.com>
2022-06-02 12:27:15 +0600

    2/6/2022

---
+++ function.dart
@@ -0,0 +1,7 @@
+int addNumber(int num1,int num2){
+  return num1+num2;
+}
+
+void main() {
+ print(addNumber(10, 10));
+}
---
+++ oop.dart
@@ -0,0 +1,11 @@
+class Person{
+  String name = "saikat";
+  int age = 24;
+  String address = "bogra";
+}
+void main(){
+  var p1 = Person();
+  print(p1);
+  print(p1.name);
+
+}
